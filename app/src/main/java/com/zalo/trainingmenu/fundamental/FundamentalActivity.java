ee9be3bcdf940b6fe5d61e4e0af3bd00929cd2e8
dtrung98 <trung17dinhle@gmail.com>
2019-10-01 18:12:40 +0700

    new camera kit section

--- app/src/main/java/com/zalo/trainingmenu/fundamental/FundamentalActivity.java
+++ app/src/main/java/com/zalo/trainingmenu/fundamental/FundamentalActivity.java
@@ -28,6 +28,7 @@ public class FundamentalActivity extends AbsMenuActivity {
         list.add(Item.with(this).setTitle(R.string.opengl).setDescription(R.string.opengl_description).setDestinationActivityClass(OpenGLActivity.class).get());
         list.add(Item.with(this).setTitle(R.string.camera).setDescription(R.string.camera_description).setDestinationActivityClass(CameraActivity.class).get());
         list.add(Item.with(this).setTitle(R.string.cameraX).setDescription(R.string.cameraX_description).setDestinationActivityClass(CameraXActivity.class).get());
+        list.add(Item.with(this).setTitle(R.string.cameraKit).setDescription(R.string.cameraKit_description).setDestinationActivityClass(CameraXActivity.class).get());
         list.add(Item.with(this).setTitle(R.string.service).setDescription(R.string.service_description).setDestinationActivityClass(ServiceDemoActivity.class).get());
         list.add(Item.with(this).setTitle(R.string.content_provider).setDescription(R.string.content_provider_description).setDestinationActivityClass(ContentProviderDemoActivity.class).get());
         list.add(Item.with(this).setTitle(R.string.network).setDescription(R.string.network_description).setDestinationActivityClass(NetworkMenuActivity.class).get());
