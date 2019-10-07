4b6c095527879776384762d24e4fd821192d8c45
dtrung98 <trung17dinhle@gmail.com>
2019-10-07 14:42:37 +0700

    prepare to modulize

--- app/src/main/java/com/zalo/trainingmenu/fundamental/camera/CameraTextureView.java
+++ app/src/main/java/com/zalo/trainingmenu/fundamental/camera/CameraTextureView.java
@@ -9,6 +9,7 @@ import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureFailure;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.TotalCaptureResult;
@@ -29,6 +30,10 @@ import java.util.Comparator;
 import java.util.List;
 
 import es.dmoral.toasty.Toasty;
+
+import static android.hardware.camera2.CameraCharacteristics.CONTROL_AE_AVAILABLE_ANTIBANDING_MODES;
+import static android.hardware.camera2.CameraMetadata.CONTROL_AE_ANTIBANDING_MODE_50HZ;
+import static android.hardware.camera2.CaptureRequest.CONTROL_AE_ANTIBANDING_MODE;
 
 public class CameraTextureView extends TextureView {
     CameraDevice mCameraDevice;
@@ -196,7 +201,8 @@ public class CameraTextureView extends TextureView {
             // Khởi tạo CaptureRequestBuilder từ cameraDevice với template truyền vào là
             // "CameraDevice.TEMPLATE_PREVIEW"
             // Với template này thì CaptureRequestBuilder chỉ thực hiện View mà thôi
-            mPreviewCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            mPreviewCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+            mPreviewCaptureRequestBuilder.set(CONTROL_AE_ANTIBANDING_MODE, CONTROL_AE_ANTIBANDING_MODE_50HZ);
 
             // Thêm đích đến cho dữ liệu lấy về từ Camera
             // Đích đến này phải nằm trong danh sách các đích đến của dữ liệu
